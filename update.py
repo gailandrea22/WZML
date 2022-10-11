329e120aae66b5c55ebbaab003b8e409831f4435
gailandrea22 <115567811+gailandrea22@users.noreply.github.com>
2022-10-11 15:43:20 +0100

    Update update.py

--- update.py
+++ update.py
@@ -20,16 +20,16 @@ try:
     try:
         res = rget(CONFIG_FILE_URL)
         if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
+            with open('config1.env', 'wb+') as f:
                 f.write(res.content)
         else:
-            log_error(f"Failed to download config.env {res.status_code}")
+            log_error(f"Failed to download config1.env {res.status_code}")
     except Exception as e:
         log_error(f"CONFIG_FILE_URL: {e}")
 except:
     pass
 
-load_dotenv('config.env', override=True)
+load_dotenv('config1.env', override=True)
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
